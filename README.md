bc4660a4993f951ae8474af0e5930dfa17b0a428
Super Cool Ninja <samir932205@gmail.com>
2021-08-21 19:35:29 +0200

    check if the renderer is not null

---
+++ README.md
@@ -0,0 +1,21 @@
+# Simple Clicker game made using SDL2 with CPP (WIP).
+I made a remake of my love2d clicker game project using SDL2 with CPP for learning how SDL2 work (this project is still in progress).
+
+## Screen :
+
+![GameDesign](https://cdn.discordapp.com/attachments/554479498721099787/878693568590512178/unknown.png)
+
+
+## Description of the game logic :
+You have a circle shape that pops pup where you must click to increase your score.
+
+The challenge for you will be to click as fast as you can on the shape that spawns all with a timer that shows you the elapsed time.
+
+Each time you click on the circular shape, the color of the shape will change randomly as well as its position.
+Good luck and have fun.
+
+## Preview : 
+
+
+
+--------------||by Super.Cool.Ninja||--------------
--- src/GameCore.cpp
+++ src/GameCore.cpp
@@ -9,6 +9,7 @@ int const MAX_RADIUS = 100;
 int const MAX_WIDTH = 640 / 2;
 int const MAX_HEIGHT = 580 / 2;
 
+
 void BeginGame()
 {
     //INIT RANDOM VALUE TO 0 : 
@@ -25,68 +26,72 @@ void BeginGame()
     Main_Window = SDL_CreateWindow("Clicker Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 580, 0);
     Main_Renderer = SDL_CreateRenderer(Main_Window, -1, SDL_RENDERER_ACCELERATED);
 
-    auto xMouse = 0;
-    auto yMouse = 0;
-    auto canClose = false;
-    auto randomPosX = 100;
-    auto randomPosY = 100;
-    auto radius = 100;
-
-    while (!canClose)
+    if (Main_Window != nullptr && Main_Renderer != nullptr)
     {
-        //Mouse Event :
-        SDL_WaitEvent(&event);
-        SDL_PumpEvents(); 
-        buttons = SDL_GetMouseState(&xMouse, &yMouse);
+        SDL_Log("Clicker game window created.");
 
-        if (event.type == SDL_MOUSEBUTTONDOWN)
+        auto xMouse = 0;
+        auto yMouse = 0;
+        auto canClose = false;
+        auto randomPosX = 100;
+        auto randomPosY = 100;
+        auto radius = 100;
+
+        while (!canClose)
         {
-            float distRadius = gameProper.distance(xMouse, yMouse, gameProper.GetPropertyCircle().x, gameProper.GetPropertyCircle().y);
-            
-            if (distRadius < radius)
+            //Mouse Event :
+            SDL_WaitEvent(&event);
+            SDL_PumpEvents();
+            buttons = SDL_GetMouseState(&xMouse, &yMouse);
+
+            if (event.type == SDL_MOUSEBUTTONDOWN)
             {
-                SDL_Log("CIRCLE TOUCH !");
-                SDL_Log("ACTUAL POSITION : %d  %d", gameProper.GetPropertyCircle().x, gameProper.GetPropertyCircle().y);
+                float distRadius = gameProper.distance(xMouse, yMouse, gameProper.GetPropertyCircle().x, gameProper.GetPropertyCircle().y);
 
-                //Create a random position for the circle : 
-                randomPosX = rand() % MAX_WIDTH + MIN_WIDTH;
-                randomPosY = rand() % MAX_HEIGHT + MIN_WIDTH;
+                if (distRadius < radius)
+                {
+                    SDL_Log("CIRCLE TOUCH !");
 
-                //Change to a random color : 
-                colors.R = rand() % 256;
-                colors.G = rand() % 256;
-                colors.B = rand() % 256;
+                    //Create a random position for the circle : 
+                    randomPosX = rand() % MAX_WIDTH + MIN_WIDTH;
+                    randomPosY = rand() % MAX_HEIGHT + MIN_WIDTH;
 
-                //Change to a random radius : 
-                radius = (rand() % (MAX_RADIUS - MIN_RADIUS) + MIN_RADIUS);
+                    //Change to a random color : 
+                    colors.R = rand() % 256;
+                    colors.G = rand() % 256;
+                    colors.B = rand() % 256;
+
+                    //Change to a random radius : 
+                    radius = (rand() % (MAX_RADIUS - MIN_RADIUS) + MIN_RADIUS);
+                }
             }
+
+
+            //Event that allow us to check if we press the X to close : 
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                canClose = true;
+                break;
+            }
+
+            //Background color : 
+            SDL_SetRenderDrawColor(Main_Renderer, 0, 0, 0, 255);
+
+            //Clean the color render : 
+            SDL_RenderClear(Main_Renderer);
+
+            //Draw Circle : 
+            gameProper.DrawCircle(Main_Renderer, randomPosX, randomPosY, radius, colors);
+
+            //Render the circle on the screen :
+            SDL_RenderPresent(Main_Renderer);
         }
 
 
-        //Event that allow us to check if we press the X to close : 
-        switch (event.type)
-        {
-        case SDL_QUIT:
-            canClose = true;
-            break;
-        }
-
-        //Background color : 
-        SDL_SetRenderDrawColor(Main_Renderer, 0, 0, 0, 255);
-        
-        //Clean the color render : 
-        SDL_RenderClear(Main_Renderer);
-        
-        //Draw Circle : 
-        gameProper.DrawCircle(Main_Renderer, randomPosX, randomPosY, radius, colors);
-        
-        //Render the circle on the screen :
-        SDL_RenderPresent(Main_Renderer);
+        //Once we close the app, clean and free everything in the back before to close it :
+        SDL_DestroyRenderer(Main_Renderer);
+        SDL_DestroyWindow(Main_Window);
+        SDL_Quit();
     }
-
-
-    //Once we close the app, clean and free everything in the back before to close it :
-    SDL_DestroyRenderer(Main_Renderer);
-    SDL_DestroyWindow(Main_Window);
-    SDL_Quit();
 }
